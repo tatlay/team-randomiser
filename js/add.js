4dacd796ed1d72019507255f566089259060f6ab
Tat Lay <GratitudeNow@Tat.local>
2019-04-01 15:31:35 +0100

    working array randomiser, need to split

--- js/add.js
+++ js/add.js
@@ -4,7 +4,8 @@
     let add = d.getElementById("add");
     let display = d.getElementById("display");
     let list = d.getElementById("list");
-
+    let randomiser = d.getElementById("randomiser");
+    let reset = d.getElementById("reset");
     //empty array that keeps track of things
     let store = [];
 
@@ -34,6 +35,41 @@
         });
         // append the fragment to the list
         list.appendChild(fragment);
+    });
+ 
+    randomiser.addEventListener("click", () => {
+        function randomFunc(shuffleArr) {      
+        let l = shuffleArr.length, temp, index;  
+        while (l > 0) {  
+           index = Math.floor(Math.random() * l);  
+           l--;  
+           temp = shuffleArr[l];          
+           shuffleArr[l] = shuffleArr[index];          
+           shuffleArr[index] = temp;      
+        }    
+        return shuffleArr;    
+        }        
+        randomFunc(store); 
 
+        // create a fragment
+        let fragment = d.createDocumentFragment();
+        // empty the list HTML
+        list.textContent = "";
+
+        // for each stored string
+        store.forEach(string => {
+            // create a list item
+            let li = d.createElement("li");
+            // set its text to the current string
+            li.textContent = string;
+            // set its class
+            li.classList.add("list-group-item");
+            // append it to the fragment
+            fragment.appendChild(li);
+        });
+
+        // append the fragment to the list
+        list.appendChild(fragment);
+    
     });
 })(document);
