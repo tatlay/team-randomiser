dcf22075956c012a14c1ac4619e1ec0af258fe40
Tat Lay <tatlay@me.com>
2019-04-01 17:59:40 +0100

    all functions working

--- js/add.js
+++ js/add.js
@@ -3,11 +3,12 @@
     let players = d.getElementById("players");
     let add = d.getElementById("add");
     let display = d.getElementById("display");
-    let list = d.getElementById("list");
+    let list1 = d.getElementById("teamList1");
+    let list2 = d.getElementById("teamList2");
     let randomiser = d.getElementById("randomiser");
     let reset = d.getElementById("reset");
     //empty array that keeps track of things
-    let store = [];
+    let store = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
 
     // when add is clicked
     add.addEventListener("click", () => {
@@ -20,7 +21,7 @@
 
         let fragment = d.createDocumentFragment();
         // empty the list in HTML
-        list.textContent = "";
+        list1.textContent = "";
 
         // for each stored string
         store.forEach(string => {
@@ -28,13 +29,11 @@
             let li = d.createElement("li");
             // set its text to the current string
             li.textContent = string;
-            // set its class
-            li.classList.add("newPlayer");
             // append it to the fragment
             fragment.appendChild(li);
         });
         // append the fragment to the list
-        list.appendChild(fragment);
+        list1.appendChild(fragment);
     });
  
     randomiser.addEventListener("click", () => {
@@ -54,22 +53,45 @@
         // create a fragment
         let fragment = d.createDocumentFragment();
         // empty the list HTML
-        list.textContent = "";
+        list1.textContent = "";
+        list2.textContent = "";
+
+        let team2 = store.slice(0,5);
+        let team1 = store.slice(5,10);
 
         // for each stored string
-        store.forEach(string => {
+        team1.forEach(t1 => {
             // create a list item
             let li = d.createElement("li");
             // set its text to the current string
-            li.textContent = string;
+            li.textContent = t1;
             // set its class
-            li.classList.add("list-group-item");
+            li.classList.add("teamOne");
             // append it to the fragment
             fragment.appendChild(li);
         });
 
         // append the fragment to the list
-        list.appendChild(fragment);
+        list1.appendChild(fragment);
+
+            // for each stored string
+        team2.forEach(t2 => {
+            // create a list item
+            let li = d.createElement("li");
+            // set its text to the current string
+            li.textContent = t2;
+            // set its class
+            li.classList.add("teamTwo");
+            // append it to the fragment
+            fragment.appendChild(li);
+        });
+
+        // append the fragment to the list
+        list2.appendChild(fragment);
+
+        console.log(team1);
+        console.log(team2);
+        
     
     });
 })(document);
